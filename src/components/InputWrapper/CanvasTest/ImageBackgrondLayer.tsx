7ade18d604570842315e69f8beb67d8ca05e7278
Soogeun Park <bwmelon97@naver.com>
2022-08-24 21:46:41 +0900

    feat: styling the VideoInput component
    See Also: #11

--- src/components/InputWrapper/CanvasTest/ImageBackgrondLayer.tsx
+++ src/components/InputWrapper/CanvasTest/ImageBackgrondLayer.tsx
@@ -8,7 +8,7 @@ function ImageBackgroundLayer() {
     return (
         <div
             className={`
-                bg-cover bg-center bg-slate-50 
+                bg-contain bg-no-repeat bg-center bg-slate-50 
                 absolute top-0 bottom-0 left-0 right-0 
                 transition-all duration-200 ease-linear
             `}
